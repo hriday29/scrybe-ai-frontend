31ed751c566c9f0b1e441ed24f93fa7d7ec84de4
hriday29 <vighriday@gmail.com>
2025-08-07 03:26:37 +0530

    fix for index.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // <-- ADD THIS LINE HERE
 import App from './App';
 
 // Create root using the root element in your HTML
