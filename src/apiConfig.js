e263241812cc82a56741d0ee99280c29b5e1041e
hriday29 <vighriday@gmail.com>
2025-11-17 02:46:57 +0530

    Redirect fix

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 // In development, we use relative URLs that will be proxied
 // In production, we use the full URL (without /api since routes already include it)
 const isDevelopment = process.env.NODE_ENV === 'development';
-const API_BASE_URL = isDevelopment ? '/api' : 'https://www.scrybeai.app';
+const API_BASE_URL = isDevelopment ? 'http://localhost:8080' : 'https://www.scrybeai.app';
 
 export { API_BASE_URL };
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://www.scrybeai.app',
+      target: 'http://localhost:8080',
       changeOrigin: true,
       secure: false,
       pathRewrite: {
